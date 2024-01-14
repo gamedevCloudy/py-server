3a06d38c61c190783b9c3aac00918c803cd1ef80
Aayush Chaudhary <acaayush1111@gmail.com>
2024-01-14 18:42:16 +0530

    logout

--- project/auth.py
+++ project/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash, jsonify
+from flask import Blueprint, render_template, redirect, url_for, request
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db 
-from flask_login import login_user
+from flask_login import login_user, login_required, logout_user
 
 
 auth = Blueprint('auth', __name__)
@@ -56,7 +56,17 @@ def signup_post():
     return signup_success
 
 
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return logging_out
+
 user_exists = """<article hx-get="/login" hx-target="#swappable" hx-trigger="load delay:1200ms" hx-swap="innerHTML"><p aria-busy="true">User already exists.</p></article>"""
 signup_success = """<article hx-get="/login" hx-target="#swappable" hx-trigger="load delay:1200ms" hx-swap="innerHTML"><p aria-busy="true">Account created, please login.</p></article>"""
 
 invalid_credentails = """<article hx-get="/login" hx-target="#swappable" hx-trigger="load delay:1200ms" hx-swap="innerHTML"><p aria-busy="true">Invalid credentials.</p></article>"""
+
+
+logging_out= """<article hx-get="/home" hx-target="#swappable" hx-trigger="load delay:1200ms" hx-swap="innerHTML"><p aria-busy="true">Logging out...</p></article>"""
--- project/main.py
+++ project/main.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-
+from flask_login import login_required, current_user
 # app = Flask(__name__)
 main = Blueprint('main', __name__)
 
@@ -8,9 +8,14 @@ main = Blueprint('main', __name__)
 def home():
     return render_template('index.html')
 
+@main.route('/home')
+def home_logout():
+    return render_template('home.html')
+
 @main.route('/profile')
+@login_required
 def profile(): 
-    return render_template('profile.html')
+    return render_template('profile.html', name=current_user.name)
 
 @main.route('/levels')
 def levels():
